522ab329c855fe2eecb303d04f86c7d99a57c84d
Labu1011 <67023054+Labu1011@users.noreply.github.com>
2021-03-14 20:16:59 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const cardContainer = document.querySelector(".card");
+const button = document.querySelector(".search-btn");
+
+const getCountryData = (name) => {
+  fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const countryData = data[0];
+      const html = `
+        <img
+        src="${countryData.flag}"
+        alt="Flag"
+        class="country-flag"
+        />
+        <div class="info">
+        <h3 class="country-name">${countryData.name}</h3>
+        <p class="country-cap">🏛️ ${countryData.capital}</p>
+        <p class="country-pop">👪 ${(countryData.population / 1000000).toFixed(
+          1
+        )}M</p>
+        <p class="country-currency">💰 ${countryData.currencies[0].name}</p>
+        <p class="country-lang">🗣️ ${countryData.languages[0].name}</p>
+        </div>`;
+      cardContainer.insertAdjacentHTML("beforeend", html);
+    });
+};
+
+button.addEventListener("click", () => {
+  const text = document.querySelector(".country-search").value;
+  getCountryData(text);
+});
